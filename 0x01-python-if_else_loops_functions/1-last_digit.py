f2ef8caef04327f9524d2dd5f277813c25a07d3a
Holden Grissett <holdeng96@gmail.com>
2017-01-03 21:59:57 +0000

    correct negative number calculation

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,15 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    last = -number % 10
+    last = -(-number % 10)
 else:
     last = number % 10
 if last > 5:
-    print("Last digit of", number, "is", last, "and is greater than 5")
-elif last < 6 and last > 0:
-    print("Last digit of", number, "is", last, "and is less than 6 and not 0")
+    print("Last digit of {:d}".format(number), end=" ")
+    print("is {:d} and is greater than 5".format(last))
+elif last < 6 and last != 0:
+    print("Last digit of {:d}".format(number), end=" ")
+    print("is {:d} and is less than 6 and not 0".format(last))
 else:
-    print ("Last digit of", number, "is", last, "and is 0")
+    print("Last digit of {:d}", end=" ")
+    print("is {:d} and is 0".format(last))
