32bbbcc0787ddef2070226f024baf2b370511b41
Holden Grissett <holdeng96@gmail.com>
2017-01-03 22:51:01 +0000

    add file 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(0, 9):
+    for j in range(0, 10):
+        if i < j and i + j != 17:
+            print("{:d}{:d}, ".format(i, j), end="")
+else:
+    print("98")
