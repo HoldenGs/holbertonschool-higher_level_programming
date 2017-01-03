eeee7d8b93a1d1435508edaefba6f82f963113fd
Holden Grissett <holdeng96@gmail.com>
2017-01-03 23:42:32 +0000

    fix FizzBuzz not showing up

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(0, 101):
+    for i in range(1, 101):
+        num = i
         if i % 3 == 0:
             num = "Fizz"
         elif i % 5 == 0:
             num = "Buzz"
-        elif i % 5 == 0 and i % 3 == 0:
+        if i % 5 == 0 and i % 3 == 0:
             num = "FizzBuzz"
-        else:
-            num = i
         print("{}".format(num), end=" ")
