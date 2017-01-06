d1fa5104c19fba425f0b7e456715fc00a35c515d
Holden Grissett <holdeng96@gmail.com>
2017-01-06 01:08:22 +0000

    import all of sys

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-from sys import argv, exit
+import sys
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    if len(argv) != 4:
+    if len(sys.argv) != 4:
         print("./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    a = argv[1]
-    op = argv[2]
-    b = argv[3]
+    a = sys.argv[1]
+    op = sys.argv[2]
+    b = sys.argv[3]
     if op == '+':
         result = add(int(a), int(b))
     elif op == '-':
