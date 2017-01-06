0c4edf679986bb80cbe3cb314b0d6bd39599785c
Holden Grissett <holdeng96@gmail.com>
2017-01-06 00:30:00 +0000

    remove if statement

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,8 +3,7 @@ if __name__ == "__main__":
     from sys import argv
     length = len(argv)
     sum = 0
-    if length > 2:
-        for i in range(1, length):
-            nextnum = int(argv[i])
-            sum += nextnum
-        print("{:d}".format(sum))
+    for i in range(1, length):
+        nextnum = int(argv[i])
+        sum += nextnum
+    print("{:d}".format(sum))
