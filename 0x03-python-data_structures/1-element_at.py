a4c4e7b58397b5e330fca0c1553400c4aa88a33e
Holden Grissett <holdeng96@gmail.com>
2017-01-10 23:18:21 +0000

    properly handle out of range indexes

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if my_list[idx]:
+        if len(my_list) - 1 < idx:
+            return "None"
         return my_list[idx]
-    else:
-        return "None"
