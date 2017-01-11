08480f6d615399cde8edd80e23895bd69c1f5134
Holden Grissett <holdeng96@gmail.com>
2017-01-11 01:30:22 +0000

    no more error on empty string

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    first_letter = sentence[0]
     if len(sentence) == 0:
         first_letter = None
+    else:
+        first_letter = sentence[0]
     t = len(sentence), first_letter
     return t
