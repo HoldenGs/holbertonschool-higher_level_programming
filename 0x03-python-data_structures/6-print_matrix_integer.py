c5a9cb0caafaf0759fb9a261cc69dc9522eaf696
Holden Grissett <holdeng96@gmail.com>
2017-01-11 00:52:29 +0000

    change to one loop line

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        string = " ".join([format(x) for x in i])
-        print(string)
+        print(" ".join([format(x) for x in i]))
