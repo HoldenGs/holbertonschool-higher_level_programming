388f5f7bb0819a84c6d43af81b127dd0d1bc36c7
Holden Grissett <holdeng96@gmail.com>
2017-01-13 03:44:51 +0000

    rehaul 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def best_score(my_dict):
-     if my_dict is None:
-          return None
-     value = list(my_dict.values())
-     key = list(my_dict.keys())
-     return key[value.index(max(value))]
+    if my_dict is None:
+        return None
+    sum = 0
+    for v in my_dict.values():
+        if type(v) != int:
+            sum += 1
+    if sum == len(my_dict):
+        return None
+    v = list(my_dict.values())
+    return list(my_dict.keys())[v.index(max(v))]
