fe66650389bb43ff85b8756a7c5ce388a74265c2
Holden Grissett <holdeng96@gmail.com>
2017-01-13 23:08:20 +0000

    add file 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    try:
+        for n in range(0, x):
+            print("{}".format(my_list[n]), end="")
+            i += 1
+        print()
+        return i
+    except:
+        print()
+        return i
