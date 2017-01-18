4cbef7399f9eaa33f74d353902969d994e3aed5b
Holden Grissett <holdeng96@gmail.com>
2017-01-18 22:15:07 +0000

    add files for 3

---
+++ 0x06-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+3-say_my_name
+
+1 function:
+say_my_name
+"""
+def say_my_name(first_name, last_name=""):
+    """
+    say_my_name: print out first_name and last_name
+    first_name: string
+    last_name: string
+
+    exceptions:
+    TypeError: if either input is not a string
+    """
+    if isinstance(first_name, str) is False:
+        raise TypeError("first_name must be a string")
+    if isinstance(last_name, str) is False:
+        raise TypeError("last_name must be a string")
+    try:
+        print("{:s} {:s}".format(first_name, last_name))
+    except:
+        raise
