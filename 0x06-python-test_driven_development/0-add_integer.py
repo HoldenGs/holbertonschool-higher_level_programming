37057d9f598b8652aaefb7180e57a8ecdffe05d7
Holden Grissett <holdeng96@gmail.com>
2017-01-18 01:00:37 +0000

    add files for 0

---
+++ 0x06-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+0-add_integer
+
+1 Function:
+add_integer
+"""
+
+def add_integer(a, b):
+    """
+    add_integer: adds two integers
+    a: first integer
+    b: second integer
+
+    converts floats to ints; raises exceptions for non-number input
+    """
+    try:
+        if isinstance(a, (int, float)) is False:
+            raise TypeError("a must be an integer")
+        if isinstance(b, (int, float)) is False:
+            raise TypeError("b must be an integer")
+        c = int(a) + int(b)
+        return c
+    except:
+        raise
---
+++ 0x06-python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+This project is for learning how to make great tests using doctests.
