581eaa39441d96138a17872f2772ee102e7d3b34
Holden Grissett <holdeng96@gmail.com>
2017-01-24 00:31:55 +0000

    add 0 optional instantiation for size

--- 0x07-python-classes/4-square.py
+++ 0x07-python-classes/4-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 class Square:
-    def __init__(self, size):
+    def __init__(self, size=0):
         if isinstance(size, int) is False:
             raise TypeError("size must be an integer")
         if size < 0:
