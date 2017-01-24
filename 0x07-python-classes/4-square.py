39b15f2e838e4deef0c981cf6a3b5ab2b417f753
Holden Grissett <holdeng96@gmail.com>
2017-01-24 20:18:24 +0000

    add documentation

--- 0x07-python-classes/4-square.py
+++ 0x07-python-classes/4-square.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
+"""
+4-square
 
-
+1 class:
+Square
+"""
 class Square:
+    """
+    Square: Square class, represents a physical square
+    """
     def __init__(self, size=0):
+        """
+        __init__: Square initializer
+        """
         if isinstance(size, int) is False:
             raise TypeError("size must be an integer")
         if size < 0:
@@ -11,10 +21,16 @@ class Square:
 
     @property
     def size(self):
+        """
+        size: private variable getter
+        """
         return self.__size
 
     @size.setter
     def size(self, size):
+        """
+        size: private variable setter
+        """
         if isinstance(size, int) is False:
             raise TypeError("size must be an integer")
         if size < 0:
@@ -22,4 +38,7 @@ class Square:
         self.__size = size
 
     def area(self):
+        """
+        area: area of instance variable size
+        """
         return self.__size * self.__size
