4aa46c7a628c5e0ca2b914fc17e0f073f68a7437
Holden Grissett <holdeng96@gmail.com>
2017-01-24 23:21:59 +0000

    add file 8

---
+++ 0x08-python-more_classes/8-rectangle.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+
+
+class Rectangle:
+
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
+
+    def __str__(self):
+        rectangle = ""
+        rectangle = "\n".join((str(self.print_symbol) * self.__width)
+                              for i in range(self.__height))
+        return rectangle
+
+    def __repr__(self):
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if isinstance(width, int) is False:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise TypeError("width must be >= 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        if isinstance(height, int) is False:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise TypeError("height must be >= 0")
+        self.__height = height
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if type(rect_1) != Rectangle:
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if type(rect_2) != Rectangle:
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() < rect_2.area():
+            return rect_2
+        else:
+            return rect_1
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        return (self.__width + self.__height) * 2
