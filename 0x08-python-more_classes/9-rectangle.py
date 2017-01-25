34f0deaec8cf81c5e30facbdd7e43022b2bfbfbb
Holden Grissett <holdeng96@gmail.com>
2017-01-25 07:30:25 +0000

    add previous bug fixes

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -7,22 +7,32 @@ class Rectangle:
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+        if isinstance(width, int) is False:
+            raise TypeError('width must be an integer')
+        elif width < 0:
+            raise ValueError('width must be >= 0')
+        if isinstance(height, int) is False:
+            raise TypeError('height must be an integer')
+        elif height < 0:
+            raise ValueError('height must be >= 0')
+        self.__width = width
+        self.__height = height
         Rectangle.number_of_instances += 1
-
-    def __del__(self):
-        print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1
 
     def __str__(self):
         rectangle = ""
+        if self.__width == 0 or self.__height == 0:
+            return rectangle
         rectangle = "\n".join((str(self.print_symbol) * self.__width)
                               for i in range(self.__height))
         return rectangle
 
     def __repr__(self):
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
 
     @property
     def width(self):
@@ -32,8 +42,8 @@ class Rectangle:
     def width(self, width):
         if isinstance(width, int) is False:
             raise TypeError("width must be an integer")
-        if width < 0:
-            raise TypeError("width must be >= 0")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
         self.__width = width
 
     @property
@@ -44,8 +54,8 @@ class Rectangle:
     def height(self, height):
         if isinstance(height, int) is False:
             raise TypeError("height must be an integer")
-        if height < 0:
-            raise TypeError("height must be >= 0")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
         self.__height = height
 
     @classmethod
@@ -67,4 +77,6 @@ class Rectangle:
         return self.__width * self.__height
 
     def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return (self.__width + self.__height) * 2
