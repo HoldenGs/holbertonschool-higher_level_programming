56f13dadb45ebcfd2eb11fa616362688680a24a6
Holden Grissett <holdeng96@gmail.com>
2017-02-01 00:36:21 +0000

    add pep8 formatting

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         BaseGeometry.integer_validator(self, "width", width)
