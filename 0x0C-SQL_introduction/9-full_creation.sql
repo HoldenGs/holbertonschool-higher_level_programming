e2d7d4da217932f0c5833ce2d3c9efc1c73a7463
Holden Grissett <holdeng96@gmail.com>
2017-02-21 02:26:12 +0000

    remove comment

--- 0x0C-SQL_introduction/9-full_creation.sql
+++ 0x0C-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- Create a second table in hbtn_0c_0 database and insert some data into it
 -- Create a table
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
--- Insert into the second_table
+
 INSERT INTO second_table (`id`, `name`, `score`) VALUES
 (1, 'John', 10),
 (1, 'Alex', 3),
