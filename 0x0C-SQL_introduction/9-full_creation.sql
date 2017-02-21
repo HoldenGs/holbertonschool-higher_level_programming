24c13ae501c5869256e3334f13ac2b63cb8533ab
Holden Grissett <holdeng96@gmail.com>
2017-02-21 02:27:53 +0000

    add backticks

--- 0x0C-SQL_introduction/9-full_creation.sql
+++ 0x0C-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,9 @@
 -- Create a second table in hbtn_0c_0 database and insert some data into it
 -- Create a table
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table (
+`id` INT,
+`name` VARCHAR(256),
+`score` INT);
 
 INSERT INTO second_table (`id`, `name`, `score`) VALUES
 (1, 'John', 10),
