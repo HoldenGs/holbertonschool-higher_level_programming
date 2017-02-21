d1f5c493a027b218e76edc0ed9f7c63e73d9b6fa
Holden Grissett <holdeng96@gmail.com>
2017-02-21 01:24:01 +0000

    remove extra spacing

--- 0x0C-SQL_introduction/9-full_creation.sql
+++ 0x0C-SQL_introduction/9-full_creation.sql
@@ -1,9 +1,6 @@
 -- Create a second table in hbtn_0c_0 database and insert some data into it
 -- Create a table
-CREATE TABLE IF NOT EXISTS second_table (
-id INT,
-name VARCHAR(256),
-score INT);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 -- Insert John into the second_table
 INSERT INTO second_table (`id`, `name`, `score`) VALUES (1, 'John', 10);
 -- Insert Alex into the second_table
