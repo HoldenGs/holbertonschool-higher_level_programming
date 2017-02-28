27ad39f3083d231d81dfb5db8cfd8d15b05a3280
Holden Grissett <holdeng96@gmail.com>
2017-02-28 09:51:31 +0000

    add file 6

---
+++ 0x0D-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- Create a database and the table 'states'
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+id INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+name VARCHAR(256) NOT NULL
+)
