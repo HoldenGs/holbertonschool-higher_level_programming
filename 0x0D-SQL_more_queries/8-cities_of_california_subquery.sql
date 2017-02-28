0a093c9b05a68dbb6b24dba7fa91aa5624f92371
Holden Grissett <holdeng96@gmail.com>
2017-02-28 10:25:38 +0000

    add file 8

---
+++ 0x0D-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,10 @@
+-- Select all cities in California
+-- Select all cities
+SELECT id, name
+FROM cities
+WHERE state_id = (
+      SELECT id
+      FROM states
+      WHERE name = 'California'
+      )
+ORDER BY cities.id ASC;
