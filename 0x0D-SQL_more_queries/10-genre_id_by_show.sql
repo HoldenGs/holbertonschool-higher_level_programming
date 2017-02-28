8ffa545056fbf7f743df95a3af60cc5805549844
Holden Grissett <holdeng96@gmail.com>
2017-02-28 11:25:09 +0000

    only print out unique genre values

--- 0x0D-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0D-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,7 @@
--- Print out tv show titles and genre ids
+-- Print out tv show titles and genre ids that have at least one genre
 -- Natural join select
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-NATURAL JOIN tv_show_genres
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
