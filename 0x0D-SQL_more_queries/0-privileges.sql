8745571ec108e8478d1cde201fadee9b730db72a
Holden Grissett <holdeng96@gmail.com>
2017-02-28 08:07:30 +0000

    add quotes

--- 0x0D-SQL_more_queries/0-privileges.sql
+++ 0x0D-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
 -- Print out all privileges for 2 users
 -- Print out privileges
-
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
