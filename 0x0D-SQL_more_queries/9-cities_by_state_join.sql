b3effb629968fb1ec0cb764d56bc609fc63ecf24
Holden Grissett <holdeng96@gmail.com>
2017-02-28 11:05:56 +0000

    add file 9

---
+++ 0x0D-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- Print out the cities with id, city name, and state name
+-- Left join select statement
+SELECT cities.id, cities.name, states.name
+FROM cities
+LEFT JOIN states
+ON cities.state_id = states.id
+ORDER BY cities.id ASC;
