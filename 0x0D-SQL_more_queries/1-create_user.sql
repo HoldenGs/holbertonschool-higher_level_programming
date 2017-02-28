a11ca37a654b92a71c071aa36f521e441489d462
Holden Grissett <holdeng96@gmail.com>
2017-02-28 08:29:01 +0000

    remove comment

--- 0x0D-SQL_more_queries/1-create_user.sql
+++ 0x0D-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
 -- Create a new user named 'user_0d_1' with password 'user_0d_1_pwd'
--- Creates a new user if none exists
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
