14da883209a55825552f3414f0af4e600deddb8d
Holden Grissett <holdeng96@gmail.com>
2017-03-02 09:36:01 +0000

    properly display output

--- 0x0E-python-object_relational_mapping/0-select_states.py
+++ 0x0E-python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,8 @@ List all items in a states table in a database
 if __name__ == "__main__":
     from sys import argv
     import MySQLdb
-    db = MySQLdb.connect(user='%s'.format(argv[1]),
-                         passwd='%s'.format(argv[2]), db='%s'.format(argv[3]))
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     c = db.cursor()
     c.execute("""SELECT id, name FROM states ORDER BY states.id ASC""")
+    for x in c.fetchall():
+        print(x)
