d28f9f387c6e3f054b6b52e1e4199e7b2c949657
Holden Grissett <holdeng96@gmail.com>
2017-03-02 11:02:58 +0000

    add file 5

--- 0x0E-python-object_relational_mapping/5-filter_cities.py
+++ 0x0E-python-object_relational_mapping/5-filter_cities.py
@@ -9,9 +9,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     c = db.cursor()
     keywords = argv[4].split(';', 1)
-    c.execute("""SELECT id, name
+    c.execute("""SELECT name
     FROM cities
     WHERE state_id = (SELECT id FROM states WHERE name = '{}')
     ORDER BY cities.id ASC""".format(keywords[0].strip("'")))
-    for x in c.fetchall():
-        print(x)
+    print(', '.join(city[0] for city in c.fetchall()))
