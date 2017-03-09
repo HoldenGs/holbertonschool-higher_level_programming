e6b4da748352d4b2a24414deae83e5fd9d18df73
Holden Grissett <holdeng96@gmail.com>
2017-03-09 23:22:11 +0000

    add file 11

---
+++ 0x0E-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+Add a state called 'Louisiana' to the database
+"""
+
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.add(State(name='Louisiana'))
+    state = session.query(State).filter(State.name == 'Louisiana')
+    try:
+        print(state[0].id)
+    except IndexError:
+        print("Not found")
