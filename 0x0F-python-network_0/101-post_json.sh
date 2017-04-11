9b1bb8a88b494c19c188fb28ce80bfcfd3649f97
Holden Grissett <holdeng96@gmail.com>
2017-04-11 03:02:16 +0000

    remove silent option

--- 0x0F-python-network_0/101-post_json.sh
+++ 0x0F-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Execute a curl POST request using data from a file in argument #2
-curl -s -H "Content-Type: application/json" -d @"$2" "$1"
+curl -H "Content-Type: application/json" -d @"$2" "$1"
