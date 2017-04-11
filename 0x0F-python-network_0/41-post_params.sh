3b9ee8876b8bda360591d9909af84433d2132918
Holden Grissett <holdeng96@gmail.com>
2017-04-11 02:33:04 +0000

    reduce to 1 line

--- 0x0F-python-network_0/41-post_params.sh
+++ 0x0F-python-network_0/41-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Execute curl with a POST request containing some information
-curl -s -d 'email=hr@holbertonschool.com' -d \
-    'subject=I will always be here for PLD' "$1"
+curl -s -d 'email=hr@holbertonschool.com' -d 'subject=I will always be here for PLD' "$1"
