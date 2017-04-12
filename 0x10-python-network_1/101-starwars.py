288db62bae0fef4688b3345fd1a7f36794c683ce
Holden Grissett <holdeng96@gmail.com>
2017-04-12 02:52:59 +0000

    add file 101

---
+++ 0x10-python-network_1/101-starwars.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+    from sys import argv
+    import requests
+    url = 'https://swapi.co/api/people/?search={}'.format(argv[1])
+    r = requests.get(url).json()
+    while r['next'] != None:
+        people = '\n'.join(list(person['name'] for person in r['results']))
+        if r['count'] > 0:
+            people = '\n' + people
+            print('Number of result: {}{}'.format(r['count'], people))
+        r = requests.get(r['next']).json()
