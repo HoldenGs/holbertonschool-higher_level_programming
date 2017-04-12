95dc602de12ad7aef94159a143cf811e83e6a687
Holden Grissett <holdeng96@gmail.com>
2017-04-12 06:13:07 +0000

    only print first 5 results...

--- 0x10-python-network_1/103-search_twitter.py
+++ 0x10-python-network_1/103-search_twitter.py
@@ -10,8 +10,7 @@ if __name__ == '__main__':
     from sys import argv
     import requests
 
-    search = argv[3].replace('#', '%23').replace('@', '%40')
-    search_url = 'https://api.twitter.com/1.1/search/tweets.json?q=' + search
+    search_url = 'https://api.twitter.com/1.1/search/tweets.json'
     auth_url = 'https://api.twitter.com/oauth2/token'
     consumer_key = argv[1]
     consumer_secret = argv[2]
@@ -27,10 +26,15 @@ if __name__ == '__main__':
 
     access_token = r.json()['access_token']
     search_header = {'Authorization': 'Bearer {}'.format(access_token)}
-    response = requests.get(search_url, headers=search_header)
+    params = {'q': argv[3]}
+    response = requests.get(search_url, params=params, headers=search_header)
 
     tweets = []
+    count = 0
     for tweet in response.json()['statuses']:
+        count += 1
+        if count > 5:
+            break
         tweets.append('[{}] {} by {}'.format(tweet['id'], tweet['text'],
                                              tweet['user']['name']))
     print('\n'.join(tweets))
