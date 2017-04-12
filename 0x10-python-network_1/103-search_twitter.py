f8b5ebc96c92182c735c40ef0d9b43fa183c3d39
Holden Grissett <holdeng96@gmail.com>
2017-04-12 06:22:29 +0000

    try changing up the data being sent

--- 0x10-python-network_1/103-search_twitter.py
+++ 0x10-python-network_1/103-search_twitter.py
@@ -10,10 +10,7 @@ if __name__ == '__main__':
     from sys import argv
     import requests
 
-    hashtag = argv[3]
-    hashtag = hashtag.replace("#", "23")
-    search_url = "https://api.twitter.com/1.1/search/tweets.json?q=%" + hashtag
-#    search_url = 'https://api.twitter.com/1.1/search/tweets.json'
+    search_url = 'https://api.twitter.com/1.1/search/tweets.json'
     auth_url = 'https://api.twitter.com/oauth2/token'
     consumer_key = argv[1]
     consumer_secret = argv[2]
@@ -24,13 +21,13 @@ if __name__ == '__main__':
         'Authorization': 'Basic {}'
         .format(bearer_token_encoded.decode('ascii'))
     }
-    data = 'grant_type=client_credentials'
+    data = {'grant_type': 'client_credentials'}
     r = requests.post(auth_url, headers=headers, data=data)
 
     access_token = r.json()['access_token']
     search_header = {'Authorization': 'Bearer {}'.format(access_token)}
-#    params = {'q': argv[3]}
-    response = requests.get(search_url, headers=search_header)
+    params = {'q': argv[3]}
+    response = requests.get(search_url, params=params, headers=search_header)
 
     tweets = []
     count = 0
