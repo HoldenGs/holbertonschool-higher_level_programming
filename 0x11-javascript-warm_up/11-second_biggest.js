9a59a4760759d212b1302e588e85bba8d7ff0802
Holden Grissett <holdeng96@gmail.com>
2017-04-28 01:58:15 +0000

    semistandard

--- 0x11-javascript-warm_up/11-second_biggest.js
+++ 0x11-javascript-warm_up/11-second_biggest.js
@@ -3,8 +3,8 @@
 if (process.argv[3] === undefined) {
   console.log(0);
 } else {
-  const nums = process.argv.slice(2)
-  nums.sort((a, b) => b - a)
-  secondBiggest = nums[1]
+  const nums = process.argv.slice(2);
+  nums.sort((a, b) => b - a);
+  let secondBiggest = nums[1];
   console.log(secondBiggest);
 }
