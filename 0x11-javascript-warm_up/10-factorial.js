6134ff210798323ea233aec035fe3a22cb50bdb3
Holden Grissett <holdeng96@gmail.com>
2017-04-28 01:34:27 +0000

    add file 10g

---
+++ 0x11-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+function factorial (n) {
+  n = parseInt(n);
+  let product = 1;
+  for (let i = 1; i < n; i++) {
+    product = n * i;
+  }
+  console.log(product);
+}
+factorial(process.argv[2]);
