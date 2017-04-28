b4fa61e8f8a7b0206970efd0de7b0c8f0d19b692
Holden Grissett <holdeng96@gmail.com>
2017-04-28 01:09:19 +0000

    add file 7

---
+++ 0x11-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const size = parseInt(process.argv[2]);
+if (isNaN(size) === true) {
+  console.log('Missing Size');
+} else {
+    for (let i = 0; i < size; i++) {
+	let x = ''
+	for (let i = 0; i < size; i++) {
+	    x += 'X'
+	}
+	console.log(x);
+    }
+}
