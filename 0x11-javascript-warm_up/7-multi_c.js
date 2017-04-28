3642b9694630d9fda41aa71b882469d0d9d9b201
Holden Grissett <holdeng96@gmail.com>
2017-04-28 01:15:04 +0000

    add file 7

--- 0x11-javascript-warm_up/7-multi_c.js
+++ 0x11-javascript-warm_up/7-multi_c.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 
-const size = parseInt(process.argv[2]);
-if (isNaN(size) === true) {
-  console.log('Missing Size');
+const string = 'C is fun'
+const num = parseInt(process.argv[2])
+if (isNaN(num) === true) {
+  console.log('Missing number of occurrences');
 } else {
-    for (let i = 0; i < size; i++) {
-	let x = ''
-	for (let i = 0; i < size; i++) {
-	    x += 'X'
-	}
-	console.log(x);
-    }
+  for (let i = 0; i < num; i++) {
+    console.log(string);
+  }
 }
