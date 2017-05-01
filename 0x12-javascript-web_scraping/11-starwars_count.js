caaa0122f6ddc19e3905011bf2ad6ef8eb454d07
Holden Grissett <holdeng96@gmail.com>
2017-05-01 03:41:18 +0000

    add file 11

---
+++ 0x12-javascript-web_scraping/11-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+const options = {
+  url: 'http://swapi.co/api/films/',
+  method: 'GET'
+};
+
+function callback (err, res, body) {
+  const json = JSON.parse(body);
+  let count = 0;
+  for (const film of json.results) {
+    for (const character of film.characters) {
+      if (character.includes('18')) {
+        count++;
+      }
+    }
+  }
+  console.log(count);
+}
+
+request(options, callback);
