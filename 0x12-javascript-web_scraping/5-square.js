5999dae3f32dfd3fcc8faf2b1607771d536aa3b1
Holden Grissett <holdeng96@gmail.com>
2017-05-01 01:29:10 +0000

    constructor now points to Square()

--- 0x12-javascript-web_scraping/5-square.js
+++ 0x12-javascript-web_scraping/5-square.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 
 const Rectangle = require('./4-rectangle').Rectangle;
-exports.Square = function Square (size) {
+function Square (size) {
   Rectangle.call(this, size, size);
 };
+
+Square.prototype = Object.create(Rectangle.prototype);
+Square.prototype.constructor = Square;
+exports.Square = Square;
