523b1b1178a16bc958d4fe08c9cf26dfc9943312
Holden Grissett <holdeng96@gmail.com>
2017-05-01 01:34:57 +0000

    revert

--- 0x12-javascript-web_scraping/4-rectangle.js
+++ 0x12-javascript-web_scraping/4-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-exports.Rectangle = function (w, h) {
+exports.Rectangle = function Rectangle (w, h) {
   if (isNaN(parseInt(w)) === false && isNaN(parseInt(h)) === false &&
       w > 0 && h > 0) {
     this.width = w;
--- 0x12-javascript-web_scraping/5-square.js
+++ 0x12-javascript-web_scraping/5-square.js
@@ -1,6 +1,31 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle').Rectangle;
+exports.Rectangle = function (w, h) {
+  if (isNaN(parseInt(w)) === false && isNaN(parseInt(h)) === false &&
+      w > 0 && h > 0) {
+    this.width = w;
+    this.height = h;
+  }
+  this.print = function () {
+    for (let i = 0; i < this.height; i++) {
+      let x = '';
+      for (let i = 0; i < this.width; i++) {
+        x += 'X';
+      }
+      console.log(x);
+    }
+  };
+  this.rotate = function () {
+    let h = this.height;
+    this.height = this.width;
+    this.width = h;
+  };
+  this.double = function () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  };
+};
+
 function Square (size) {
   Rectangle.call(this, size, size);
 }
