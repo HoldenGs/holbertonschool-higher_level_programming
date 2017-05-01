db5ac439892b79c50ae949235f181064f270648b
Holden Grissett <holdeng96@gmail.com>
2017-05-01 01:11:18 +0000

    add file 3

---
+++ 0x12-javascript-web_scraping/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+exports.Rectangle = function Rectangle (w, h) {
+  if (isNaN(parseInt(w)) === false && isNaN(parseInt(h)) === false &&
+      w > 0 && h > 0) {
+    this.width = w;
+    this.height = h;
+  }
+  this.print = function print () {
+    for (let i = 0; i < this.height; i++) {
+      let x = '';
+      for (let i = 0; i < this.width; i++) {
+        x += 'X';
+      }
+      console.log(x);
+    }
+  };
+};
